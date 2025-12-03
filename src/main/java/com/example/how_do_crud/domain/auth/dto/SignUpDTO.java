d0101fd17868de824ed992cf286127bc20d4a578
d1ng1724 <s25004@gsm.hs.kr>
2025-12-03 20:57:00 +0900

    기존 dto를 record 형으로 수정 및 리펙터링, @Value로 만료시간 받아오기 추가, 그 외 혼선을 줄 수 있는 요소 수정

--- src/main/java/com/example/how_do_crud/domain/auth/dto/SignUpDTO.java
+++
@@ -1,4 +0,0 @@
-package com.example.how_do_crud.domain.auth.dto;
-
-public record SignUpDTO(String name, String email, String password, String role) {
-}
---
+++ src/main/java/com/example/how_do_crud/domain/auth/dto/request/SignUpReq.java
@@ -0,0 +1,13 @@
+package com.example.how_do_crud.domain.auth.dto;
+
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record SignUpReq(
+        String name,
+        String email,
+        String password,
+        List<String> roles) {
+}
